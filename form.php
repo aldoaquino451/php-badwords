fd923f0e66bb0b6d7602ee8614149505358b9641
aldoaquino451 <aldoaquino2909@gmail.com>
2023-10-26 17:56:05 +0200

    [init] file index e form

---
+++ form.php
@@ -0,0 +1,49 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- cdnjs -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/css/bootstrap.css"
+    integrity="sha512-r0fo0kMK8myZfuKWk9b6yY8azUnHCPhgNz/uWDl2rtMdWJlk7gmd9socvGZdZzICwAkMgfTkVrplDahQ07Gi0A=="
+    crossorigin="anonymous" />
+
+  <!-- titolo  -->
+  <title>PHP Form</title>
+</head>
+
+<body>
+
+  <div class="container my-4">
+    <h2 class="mb-4">Confronto tra il testo originale e il nuovo</h2>
+    <div class="row">
+
+      <div class="col-6">
+        <div class="card p-2 ">
+          <h2>Testo originale</h2>
+          <p>testo</p>
+          <p>Lunghezza del testo</p>
+        </div>
+      </div>
+
+      <div class="col-6">
+        <div class="card p-2 ">
+          <h2>Testo originale</h2>
+          <p>testo</p>
+          <p>Lunghezza del testo</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+</body>
+
+</html>
